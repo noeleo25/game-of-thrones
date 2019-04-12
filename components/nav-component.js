fc1b2632ad0f26bfc3b1f18004fdcebd01100c56
Noemi Leon <noemi.telematik213@gmail.com>
2019-04-11 19:56:54 -0500

    Change logo, image size

--- components/nav-component.js
+++ components/nav-component.js
@@ -1,16 +1,16 @@
 const NavComponent = {
     template: `
             <nav class="navbar">
-                <a href="" class="nav-logo navbar-brand">
-                    <img class="got-logo" alt="Game of Thrones" src="./assets/got-logo.png"/>
+                <a href="./" class="nav-logo navbar-brand">
+                    <img class="got-logo" alt="Game of Thrones" src="./assets/got-logo-200x88.png"/>
                 </a>
                 
                 <ul class="nav justify-content-end">
-                    <li class="nav-item ">
-                        <a class="nav-link active" href="/">Characters</a>
+                    <li class="nav-item" id="nav-1">
+                        <a class="nav-link" href="./">Characters</a>
                     </li>
-                    <li class="nav-item">
-                        <a class="nav-link active" href="/translate.html">Translate</a>
+                    <li class="nav-item" id="nav-2">
+                        <a class="nav-link" href="./translate.html">Translate</a>
                     </li>
                 </ul>
             </nav>`,
